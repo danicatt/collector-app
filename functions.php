e48abf71631a74582165958257eb1c1138e22ca0
Dani Catterill <danicatterill@gmail.com>
2020-03-24 13:56:23 +0000

    Added inital styling to collector-app and uploaded images to use.

--- functions.php
+++ functions.php
@@ -6,4 +6,16 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->query('SELECT `name`, `species`, `tvshow`, `catchphase` FROM `cartoonChars`;');
 $cartoonArray = $query->fetchAll();
 
-?>
+
+function loopCartoons($cartoonArray){
+    $return = '';
+    foreach ($cartoonArray as $character){
+        $return .= "<div class='eachCharacter'>";
+        $return .= "<h2 class='charName'>" . $character['name'] . "</h2>";
+        $return .= "<p class='cardContent'>" . "From". ': '. $character['tvshow']. "</p>";
+        $return .= "<p class='cardContent'>" . "Species". ': '. $character['species']. "</p>";
+        $return .= "<p class='cardContent'>" . "Catchphrase". ': '. $character['catchphase']. "</p>";
+        $return .= "</div>";
+    }
+        return $return;
+}
--- index.php
+++ index.php
@@ -6,22 +6,20 @@ require('functions.php');
 <html lang="en">
     <head>
         <title> Cartoon Character Collection </title>
-        <link rel="stylesheet" href="stylesheet.css" />
+        <link rel="stylesheet" type=text/css href="stylesheet.css" />
         <link rel="normalize_css" href="normalize_css.css" />
+        <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Fredoka+One&display=swap" rel="stylesheet">
     </head>
     <body>
         <div class="titleBar">
-            <h1> Cartoon Character Collection </h1>
+            <h1 class="title"> Cartoon Character Collection </h1>
         </div>
         <main>
-    <?php
-        foreach ($cartoonArray as $character){
-            echo '<h2>' . $character["name"] . '</h2>';
-            echo '<p>' . "From" . ': ' . $character["tvshow"] . '</p>';
-            echo '<p>' . "Species" . ': ' . $character["species"] . '</p>';
-            echo '<p>' . "Catchphrase" . ': '  . $character["catchphase"] . '</p>';
-        }
-    ?>
+            <div class="collectionCards">
+                <?php
+                echo loopCartoons($cartoonArray);
+                ?>
+            </div>
         </main>
     </body>
 </html>
