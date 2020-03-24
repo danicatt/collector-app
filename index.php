a9b7709ab3ce8d3b2e5b6b31ea3e4d1ac7be1a2f
Dani Catterill <danicatterill@gmail.com>
2020-03-24 09:59:13 +0000

    Displays collection items from database on page.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require('functions.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Cartoon Character Collection </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div class="titleBar">
+            <h1> Cartoon Character Collection </h1>
+        </div>
+        <main>
+        <?php
+        foreach ($cartoonArray as $character){
+            echo '<h2>' . $character["name"] . '</h2>';
+            echo '<p>' . "From" . ': ' . $character["tvshow"] . '</p>';
+            echo '<p>' . "Species" . ': ' . $character["species"] . '</p>';
+            echo '<p>' . "Catchphrase" . ': '  . $character["catchphase"] . '</p>';
+        }
+        ?>
+        </main>
+
+        <div>
+        
+        </div>
+    </body>
+</html>
+
